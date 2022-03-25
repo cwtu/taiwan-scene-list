19b12522a285a427ec9e9246644f26bbe2dc0d17
cwtu <cwtu12@gmail.com>
2022-03-25 14:01:52 -0400

    update package and var name

--- src/Scene.js
+++ src/Scene.js
@@ -1,16 +1,18 @@
 import { memo } from "react";
 
 // only render new scenes
-const Scene = memo(({ Name, Description, DescriptionDetail, Picture }) => {
-  if (!Description) Description = DescriptionDetail;
+const Scene = memo(
+  ({ ScenicSpotName, Description, DescriptionDetail, Picture }) => {
+    if (!Description) Description = DescriptionDetail;
 
-  return (
-    <li className="scene">
-      <h2>{Name}</h2>
-      <hr />
-      <p className="description">{Description}</p>
-    </li>
-  );
-});
+    return (
+      <li className="scene">
+        <h2>{ScenicSpotName}</h2>
+        <hr />
+        <p className="description">{Description}</p>
+      </li>
+    );
+  }
+);
 
 export default Scene;
